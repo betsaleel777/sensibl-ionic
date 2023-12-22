49e4c910239045329f772100b9f2e330bc1bb6ed
Ahoussou Jean-Chris Arnaud <shin.ahoussou@gmail.com>
2023-12-22 21:59:13 +0000

    last editing

--- components/MenuComponent.vue
+++ components/MenuComponent.vue
@@ -2,26 +2,26 @@
 const isOpen = ref<boolean>(false);
 const message = ref<string>("");
 const currentLang = ref<string>("fr");
-const langues = ref<{ id: string; texte: string }>([
+const langues = ref<Array<{ id: string; texte: string }>>([
   { id: "fr", texte: isoToEmoji("fr") + " Français" },
   { id: "en", texte: isoToEmoji("gb") + " English" },
   { id: "pt", texte: isoToEmoji("pt") + " Português" },
 ]);
 const currentTheme = ref<string>("def");
-const themes = ref<{ id: string; texte: string }>([
+const themes = ref<Array<{ id: string; texte: string }>>([
   { id: "def", texte: "Défaut" },
   { id: "ci", texte: isoToEmoji("ci") + " Eléphant" },
   { id: "eg", texte: isoToEmoji("eg") + " Pharaons" },
   { id: "cm", texte: isoToEmoji("cm") + " Lions Indomptables" },
   { id: "mz", texte: isoToEmoji("mz") + " Mambas" },
 ]);
-const handleLanguageChange = (event) => {
+const handleLanguageChange = (event: any) => {
   const find = langues.value.filter((langue) => langue.id === event.detail.value)[0];
   message.value = `${find.texte} est la nouvelle langue.`;
   currentLang.value = find.id;
   isOpen.value = true;
 };
-const handleThemeChange = (event) => {
+const handleThemeChange = (event: any) => {
   const find = themes.value.filter((theme) => theme.id === event.detail.value)[0];
   message.value = `${find.texte} est le nouveau thème.`;
   currentTheme.value = find.id;
